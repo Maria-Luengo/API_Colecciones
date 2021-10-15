ad50c192004bba167eff5efff14911aa4b172121
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-15 12:20:38 +0200

    Create Test.java

---
+++ ApiColecciones/src/test/Test.java
@@ -0,0 +1,15 @@
+
+package test;
+
+import genericos.ClaseGenerica;
+
+
+public class Test {
+    public static void main(String[] args) {
+        ClaseGenerica<Integer> objInt= new ClaseGenerica(15);
+        objInt.obtenerTipo();
+        
+        ClaseGenerica<String> objString = new ClaseGenerica("Maria");
+        objString.obtenerTipo();
+    }
+}
